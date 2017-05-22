a4226bec5496fd896431d53d214958e5483641bd
Student003 <student003@Antons-iMac.local>
2017-05-22 11:14:30 +0300

    Task06 -> added tests

--- Task06_ObjC/Task06_Tests/Specs/Task06_Tests.m
+++ Task06_ObjC/Task06_Tests/Specs/Task06_Tests.m
@@ -8,38 +8,57 @@
 
 #import "Kiwi.h"
 
+#import "IDPCar.h"
 #import "IDPCarWasher.h"
 #import "IDPAccountant.h"
-
-#import "IDPConstants.h"
-#import "IDPMacros.h"
+#import "IDPDirector.h"
 
 #import "NSObject+IDPExtensions.h"
 
 SPEC_BEGIN(Task06_Test)
 
 describe(@"IDPWorkerDelegate", ^{
-    __block IDPCarWasher *carWasher;
-    __block IDPAccountant *accountant;
     
-    context(@"a state the component is in", ^{
-   
+    context(@"when carWasher received money", ^{
+        __block IDPCarWasher *carWasher;
+        __block IDPCar *car;
+        
         beforeAll(^{ // Occurs once
+            car = [IDPCar object];
             carWasher = [IDPCarWasher object];
-            accountant = [IDPAccountant object];
+            car.delegate = carWasher;
         });
         
         afterAll(^{ // Occurs once
             carWasher = nil;
-            accountant = nil;
+            car = nil;
         });
         
-        it(@"should do something", ^{
-            NSUInteger money = IDPRandomWithRange(IDPCashRange);
+        it(@"delegate should receive -delegatingObjectDidGetMoney", ^{
+            [[carWasher should] receive:@selector(delegatingObjectDidGetMoney:)];
+            [car payForCarWash];
+        });
+        
+        context(@"when accountant did his work", ^{
+            __block IDPAccountant *accountant;
+            __block IDPDirector *director;
             
+            beforeAll(^{ // Occurs once
+                accountant = [IDPAccountant object];
+                director = [IDPDirector object];
+                accountant.delegate = director;
+            });
+            
+            afterAll(^{ // Occurs once
+                director = nil;
+                accountant = nil;
+            });
+            
+            it(@"delegate should receive -delegatingObjectDidGetMoney", ^{
+                [[director should] receive:@selector(delegatingObjectDidGetMoney:)];
+                [accountant processObject:carWasher];
+            });
         });
-        
-
     });
 });
 
