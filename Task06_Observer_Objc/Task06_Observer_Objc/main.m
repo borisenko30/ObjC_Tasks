56c4a4829024e12b3bf4040824e3cb2e091cfdc3
Student003 <student003@Antons-iMac.local>
2017-05-25 17:16:04 +0300

    Refactoring

---
+++ Task06_Observer_Objc/Task06_Observer_Objc/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Task06_Observer_Objc
+//
+//  Created by Student003 on 5/25/17.
+//  Copyright © 2017 Student003. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
